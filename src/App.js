f0964c11ed50de8d82911570c6ccb7562f1c4ed7
haririthanyaa <128139401+haririthanyaa@users.noreply.github.com>
2023-04-05 09:38:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Productlist from './Productlist';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Productlist products={[{no:"1",name:"Phone", price:"21,000", imageUrl:"https://s3.ap-south-1.amazonaws.com/stickon-web-mobile/production-2/products/base_images/y73-newsfCeLDyEvBu7c1sJ.png"},{no:"2",name:"Car", price:"70,00,000", imageUrl:"https://imgd.aeplcdn.com/0x0/n/cw/ec/93821/exterior-right-front-three-quarter.jpeg"}]}/>
+    </div>
+  );
+}
+
+export default App;
